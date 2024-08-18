dd956bc782a59be6b85074e5de59ab2f5559ab1d
savaliya krupal <krupalsavaliya0@gmail.com>
2024-08-18 21:46:23 +0530

    deploymenr12

--- AgroPlus-master/React-app/src/constants.js
+++ AgroPlus-master/React-app/src/constants.js
@@ -1,6 +1,6 @@
 const API_URL = process.env.NODE_ENV !== 'development'
   ? 
-   'hhttps://myportfolio-backend-phzl.onrender.com':process.env.REACT_APP_BASE_URL;
+   'https://myportfolio-backend-phzl.onrender.com':process.env.REACT_APP_BASE_URL;
 // const API_URL="http://localhost:4000"
 console.log(process.env, "API_URL");
 
