866f91070e9ec1f540efca4b701fb35ada4075b9
savaliya krupal <krupalsavaliya0@gmail.com>
2024-08-22 09:43:12 +0530

    constant.js?

--- AgroPlus-master/React-app/src/constants.js
+++ AgroPlus-master/React-app/src/constants.js
@@ -2,7 +2,7 @@ const API_URL = process.env.NODE_ENV !== 'development'
   ? 
    'https://agro-plus-backend.onrender.com':process.env.REACT_APP_BASE_URL;
 // const API_URL="http://localhost:4000"
-// console.log(process.env, "API_URL")
+
 
 
 
