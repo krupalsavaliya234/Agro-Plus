8def59781dba0f6ea228f2d79a9ac1f1ad40f4dc
savaliya krupal <krupalsavaliya0@gmail.com>
2024-08-22 12:15:27 +0530

    update

--- AgroPlus-master/React-app/src/Components3/MyProducts.jsx
+++ AgroPlus-master/React-app/src/Components3/MyProducts.jsx
@@ -7,29 +7,36 @@ import { FaHeart } from "react-icons/fa";
 import "./styles/myproduct.css";
 import API_URL from "../constants";
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-hot-toast";
+import "react-hot-toast/dist/index.css";
 
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import ClipLoader from "react-spinners/ClipLoader";
+
 const MyProducts = () => {
   const navigate = useNavigate();
   const [readMoreId, setReadMoreId] = useState(null);
   const [liked1, setLiked] = useState(false);
-
   const [products, setProducts] = useState([]);
   const [categorizedProducts, setCategorizedProducts] = useState([]);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true); // Add loading state
+
   const fetchData = async () => {
     try {
+      setLoading(true); // Start loading
       const url = API_URL + "/my-products";
       const data = { userId: localStorage.getItem("userId") };
       const response = await axios.post(url, data);
       if (response.data.products) {
         setProducts(response.data.products);
+        setLoading(false); // End loading
       }
     } catch (error) {
-      alert("Server Err.");
+      setLoading(false); // End loading
+      toast.error("Server Error.");
     }
   };
 
@@ -64,12 +71,12 @@ const MyProducts = () => {
       const data = { userId, productId };
       const response = await axios.post(url, data);
       if (response.data.message) {
-        toast.dark("Image  Liked successfully!", {
+        toast.success("Image liked successfully!", {
           position: "top-center",
         });
       }
     } catch (error) {
-      alert("Server Err.");
+      toast.error("Server Error.");
     }
   };
 
@@ -84,28 +91,25 @@ const MyProducts = () => {
   const removeProduct = async (id) => {
     try {
       const res = await axios.delete(`${API_URL}/delete-product/${id}`);
-      console.log(res.data.product);
-
       const new1 = await axios.post(
         `${API_URL}/selled-product`,
         res.data.product
       );
-      console.log(new1);
 
-      toast.success("Product Selled!", {
+      toast.success("Product sold!", {
         position: "top-right",
-        autoClose: 1500,
+        duration: 1500,
         onClose: () => {
           fetchData();
         },
       });
     } catch (error) {
       if (error.response && error.response.status === 404) {
-        toast.dark("Product Not Found!", {
+        toast.error("Product not found!", {
           position: "top-right",
         });
       } else {
-        toast.dark("Error in Delete Product !", {
+        toast.error("Error deleting product!", {
           position: "top-right",
         });
       }
@@ -123,115 +127,118 @@ const MyProducts = () => {
       </div>
       {/* <Categories handleCategory={handleCategory} /> */}
 
-      <div className="liked-main3">
-        <div className="d-flex justify-content-center flex-wrap ">
-          {categorizedProducts.length > 0 &&
-            categorizedProducts.map((item) => (
-              <div>
-                <img
-                  width="300px"
-                  height="200px"
-                  src={API_URL + "/" + item.pimage1}
-                  alt={item.pname}
-                />
-                <p className="m-2">
-                  {item.pname} | {item.category}
-                </p>
-                <h3 className="m-2 text-danger">{item.price}</h3>
-                <p className="m-2 text-success">{item.pdesc}</p>
-                <button onClick={() => removeProduct(item._id)}>Delete</button>
-              </div>
-            ))}
+      {loading ? ( // Show spinner while loading
+        <div className="spinner-container">
+          <ClipLoader color="#000" loading={loading} size={50} />
         </div>
-
-        <h5 className="my-product-title3"> My Products </h5>
-
-        <div className="  d-flex justify-content-center flex-wrap mb-5">
-          {products.length > 0 &&
-            products.map((item) => {
-              const truncatedDescription =
-                item.pdesc.length > 50
-                  ? `${item.pdesc.slice(0, 50)}...`
-                  : item.pdesc;
-
-              const isReadMore = readMoreId === item._id;
-              return (
-                <div key={item._id} className="card3 m-3 position-relative">
-
-                  <Carousel
-                    autoPlay={true}
-                    animation="slide"
-                    showStatus={false}
-                    showThumbs={false}
-                    interval={3000}
-                    infiniteLoop={true}
-                    stopOnHover={true}
-                    navButtonsAlwaysVisible={true}
-                    navButtonsProps={{
-                      style: {
-                        backgroundColor: "#fff",
-                        color: "#494949",
-                        borderRadius: 0,
-                        marginTop: -22,
-                        height: "104px",
-                      },
-                    }}
-                  >
-                    {item.images.map((image, imageIndex) => (
-                      <div
-                        key={imageIndex}
-                        onClick={() => handleProduct(item._id)}
-                      >
-                        <img
-                          width="300px"
-                          height="200px"
-                          padding="0px"
-                          className="useradded-img3"
-                          src={API_URL + "/" + image}
-                          alt={item.pname}
-                        />
-                      </div>
-                    ))}
-                  </Carousel>
-                  <h4 className="m-2 price-text"> Rs. {item.price}/- </h4>
+      ) : (
+        <div className="liked-main3">
+          <div className="d-flex justify-content-center flex-wrap ">
+            {categorizedProducts.length > 0 &&
+              categorizedProducts.map((item) => (
+                <div key={item._id}>
+                  <img
+                    width="300px"
+                    height="200px"
+                    src={item.pimage1}
+                    alt={item.pname}
+                  />
                   <p className="m-2">
-                    {" "}
-                    {item.pname} | <span className="cat3">{item.category} </span>
+                    {item.pname} | {item.category}
                   </p>
-                  <p className="m-2">
-                    {isReadMore ? item.pdesc : truncatedDescription}{" "}
-                    {item.pdesc.length > 50 && (
-                      <p
-                        onClick={() =>
-                          setReadMoreId(isReadMore ? null : item._id)
-                        }
-                        className="read-more-btn3"
-                      >
-                        {isReadMore ? "Read Less" : "Read More"}
-                      </p>
-                    )}
-                  </p>
-
-                  <div className="myPrductButton3">
-                    <button
-                      className="myproductButton3"
-                      onClick={() => removeProduct(item._id)}
-                    >
-                      Delete
-                    </button>
-                    <button
-                      className="myproductButton31"
-                      onClick={() => modifyProduct(item._id)}
-                    >
-                      Update
-                    </button>
-                  </div>
+                  <h3 className="m-2 text-danger">{item.price}</h3>
+                  <p className="m-2 text-success">{item.pdesc}</p>
+                  <button onClick={() => removeProduct(item._id)}>Delete</button>
                 </div>
-              );
-            })}
+              ))}
+          </div>
+
+          <h5 className="my-product-title3"> My Products </h5>
+
+          <div className="d-flex justify-content-center flex-wrap mb-5">
+            {products.length > 0 &&
+              products.map((item) => {
+                const truncatedDescription =
+                  item.pdesc.length > 50
+                    ? `${item.pdesc.slice(0, 50)}...`
+                    : item.pdesc;
+
+                const isReadMore = readMoreId === item._id;
+                return (
+                  <div key={item._id} className="card3 m-3 position-relative">
+                    <Carousel
+                      autoPlay={true}
+                      animation="slide"
+                      showStatus={false}
+                      showThumbs={false}
+                      interval={3000}
+                      infiniteLoop={true}
+                      stopOnHover={true}
+                      navButtonsAlwaysVisible={true}
+                      navButtonsProps={{
+                        style: {
+                          backgroundColor: "#fff",
+                          color: "#494949",
+                          borderRadius: 0,
+                          marginTop: -22,
+                          height: "104px",
+                        },
+                      }}
+                    >
+                      {item.images.map((image, imageIndex) => (
+                        <div
+                          key={imageIndex}
+                          onClick={() => handleProduct(item._id)}
+                        >
+                          <img
+                            width="300px"
+                            height="200px"
+                            padding="0px"
+                            className="useradded-img3"
+                            src={API_URL + "/" + image}
+                            alt={item.pname}
+                          />
+                        </div>
+                      ))}
+                    </Carousel>
+                    <h4 className="m-2 price-text"> Rs. {item.price}/- </h4>
+                    <p className="m-2">
+                      {item.pname} | <span className="cat3">{item.category}</span>
+                    </p>
+                    <p className="m-2">
+                      {isReadMore ? item.pdesc : truncatedDescription}{" "}
+                      {item.pdesc.length > 50 && (
+                        <p
+                          onClick={() =>
+                            setReadMoreId(isReadMore ? null : item._id)
+                          }
+                          className="read-more-btn3"
+                        >
+                          {isReadMore ? "Read Less" : "Read More"}
+                        </p>
+                      )}
+                    </p>
+
+                    <div className="myPrductButton3">
+                      <button
+                        className="myproductButton3"
+                        onClick={() => removeProduct(item._id)}
+                      >
+                        Delete
+                      </button>
+                      <button
+                        className="myproductButton31"
+                        onClick={() => modifyProduct(item._id)}
+                      >
+                        Update
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
         </div>
-        <ToastContainer />
-      </div>
+      )}
     </div>
   );
 };
