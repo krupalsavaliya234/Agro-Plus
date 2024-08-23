9389749742e4babb1d41c512e2aa61c3f184f7e3
savaliya krupal <krupalsavaliya0@gmail.com>
2024-08-23 18:26:21 +0530

    update

--- AgroPlus-master/React-app/src/App.js
+++ AgroPlus-master/React-app/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Home from "./Components3/Home.jsx";
 import Header from "./Components3/Header.jsx";
 import { useTranslation } from 'react-i18next';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"; 
 function App() {
  
   return (
     <div className="App">
+    <SpeedInsights>
+
       <Header/>
       <Home />
+    </SpeedInsights>
       
     </div>
   );
