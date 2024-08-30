0c63d2f1dfb57fec321ff830b02b591c412bb547
savaliya krupal <krupalsavaliya0@gmail.com>
2024-08-30 13:54:55 +0530

    update

--- AgroPlus-master/React-app/src/Components3/LikedProducts.jsx
+++ AgroPlus-master/React-app/src/Components3/LikedProducts.jsx
@@ -1,94 +1,81 @@
 import { useEffect, useState } from "react";
 import Header from "./Header";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Categories from "./Categories";
-import { FaHeart } from "react-icons/fa";
-import "./styles/likeproduct.css";
-import API_URL from "../constants";
-import { DotLoader, BeatLoader } from "react-spinners";
+import { DotLoader } from "react-spinners";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./styles/likeproduct.css";
+import API_URL from "../constants";
+
 function LikedProducts() {
   const navigate = useNavigate();
-
-  const [products, setproducts] = useState([]);
-  const [cproducts, setcproducts] = useState([]);
-  const [search, setsearch] = useState("");
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [search, setSearch] = useState("");
   const [readMoreId, setReadMoreId] = useState(null);
-const [loader,setloader]=useState(false);
-  useEffect(() => {
-      if (!localStorage.getItem('token')) {
-          navigate('/login')
-      }
-  }, [])
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setloader(true);
-    const url = API_URL + "/liked-products";
-    let data = { userId: localStorage.getItem("userId") };
-    // console.log()
-    axios
-      .post(url, data)
+    if (!localStorage.getItem("token")) {
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    setLoading(true);
+    const url = `${API_URL}/liked-products`;
+    const data = { userId: localStorage.getItem("userId") };
+
+    axios.post(url, data)
       .then((res) => {
         if (res.data.products) {
-          setloader(true);
-
-          setproducts(res.data.products);
+          setProducts(res.data.products);
+          setFilteredProducts(res.data.products);
+          setLoading(false);
         }
       })
       .catch((err) => {
-        setloader(true);
-        alert("Server Err.");
+        setLoading(false);
+        alert("Server Error.");
       });
   }, []);
 
-  const handlesearch = (value) => {
-    setsearch(value);
-  };
-
-  const handleClick = () => {
-    let filteredProducts = products.filter((item) => {
-      if (
-        item.pname.toLowerCase().includes(search.toLowerCase()) ||
-        item.pdesc.toLowerCase().includes(search.toLowerCase()) ||
-        item.category.toLowerCase().includes(search.toLowerCase())
-      ) {
-        return item;
-      }
-    });
-    setcproducts(filteredProducts);
+  const handleSearch = (value) => {
+    setSearch(value);
+    const filtered = products.filter((item) =>
+      item.pname.toLowerCase().includes(value.toLowerCase()) ||
+      item.pdesc.toLowerCase().includes(value.toLowerCase()) ||
+      item.category.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredProducts(filtered);
   };
 
   const handleCategory = (value) => {
-    let filteredProducts = products.filter((item, index) => {
-      if (item.category == value) {
-        return item;
-      }
-    });
-    setcproducts(filteredProducts);
+    const filtered = products.filter((item) =>
+      item.category === value
+    );
+    setFilteredProducts(filtered);
   };
 
   const handleLike = (productId) => {
-    let userId = localStorage.getItem("userId");
-
-    const url = API_URL + "/like-product";
+    const userId = localStorage.getItem("userId");
+    const url = `${API_URL}/like-product`;
     const data = { userId, productId };
-    axios
-      .post(url, data)
+
+    axios.post(url, data)
       .then((res) => {
         if (res.data.message) {
           alert("Liked.");
         }
       })
-      .catch((err) => {
-        alert("Server Err.");
+      .catch(() => {
+        alert("Server Error.");
       });
   };
 
   const handleProduct = (id) => {
-    // alert("clicked")
-    navigate("/product/" + id);
+    navigate(`/product/${id}`);
   };
 
   return (
@@ -96,52 +83,35 @@ const [loader,setloader]=useState(false);
       <div className="like-header2">
         <Header
           search={search}
-          handlesearch={handlesearch}
+          handlesearch={handleSearch}
           handleProduct={handleProduct}
         />
       </div>
-      {/* <Categories handleCategory={handleCategory} /> */}
       <div className="liked-main2">
-        {/* <h5> SEARCH RESULTS </h5> */}
         <div className="d-flex justify-content-center flex-wrap">
-          {cproducts &&
-            products.length > 0 &&
-            cproducts.map((item, index) => {
-              const truncatedDescription =
-                item.pdesc.length > 50
-                  ? `${item.pdesc.slice(0, 50)}...`
-                  : item.pdesc;
-
+          {loading ? (
+            <DotLoader color="#000" loading={loading} size={50} />
+          ) : (
+            filteredProducts.map((item) => {
               const isReadMore = readMoreId === item._id;
               return (
-                <div key={item._id} className="card2 m-3 ">
-
+                <div key={item._id} className="card2 m-3">
                   <Carousel
-                    autoPlay={true}
+                    autoPlay
                     animation="slide"
                     showStatus={false}
                     showThumbs={false}
                     interval={3000}
-                    infiniteLoop={true}
-                    stopOnHover={true}
-                    navButtonsAlwaysVisible={true}
-                    navButtonsProps={{
-                      style: {
-                        backgroundColor: "#fff",
-                        color: "#494949",
-                        borderRadius: 0,
-                        marginTop: -22,
-                        height: "104px",
-                      },
-                    }}
+                    infiniteLoop
+                    stopOnHover
                   >
-                    {item.images.map((image, imageIndex) => (
-                      <div key={imageIndex}>
+                    {item.images.map((image, index) => (
+                      <div key={index}>
                         <img
                           width="300px"
                           height="200px"
                           className="useradded-img2"
-                          src={ image}
+                          src={image}
                           alt={item.pname}
                           onClick={() => handleProduct(item._id)}
                         />
@@ -149,93 +119,24 @@ const [loader,setloader]=useState(false);
                     ))}
                   </Carousel>
                   <p className="m-2">
-                    {" "}
-                    {item.pname} | {item.category}{" "}
+                    {item.pname} | {item.category}
                   </p>
-                  <h3 className="m-2 text-danger"> {item.price} </h3>
-                  <p className="m-2 text-success"> {item.pdesc} </p>
-                </div>
-              );
-            })}
-        </div>
-
-        <h5 className="p-header2"> Liked Products </h5>
-
-        <div className=" likeItem2 likeCart2">
-          {products &&
-            products.length > 0 &&
-            products.map((item, index) => {
-              const truncatedDescription =
-                item.pdesc.length > 50
-                  ? `${item.pdesc.slice(0, 50)}...`
-                  : item.pdesc;
-
-              const isReadMore = readMoreId === item._id;
-              return (
-                <div key={item._id} className="card2 m-4">
-                 
-                  <Carousel
-                    autoPlay={true}
-                    animation="slide"
-                    showStatus={false}
-                    showThumbs={false}
-                    interval={3000}
-                    infiniteLoop={true}
-                    stopOnHover={true}
-                    navButtonsAlwaysVisible={true}
-                    navButtonsProps={{
-                      style: {
-                        backgroundColor: "#fff",
-                        color: "#494949",
-                        borderRadius: 0,
-                        marginTop: -22,
-                        height: "104px",
-                      },
-                    }}
-                  >
-                    {item.images.map((image, imageIndex) => (
-                      <div
-                        key={imageIndex}
-                        onClick={() => handleProduct(item._id)}
+                  <h3 className="m-2 text-danger">Rs. {item.price}</h3>
+                  <p className="m-2 text-success">
+                    {isReadMore ? item.pdesc : `${item.pdesc.slice(0, 50)}...`}
+                    {item.pdesc.length > 50 && (
+                      <span
+                        onClick={() => setReadMoreId(isReadMore ? null : item._id)}
+                        className="read-more-btn2"
                       >
-                        <img
-                          width="300px"
-                          height="200px"
-                          className="useradded-img2"
-                          src={ image}
-                          alt={item.pname}
-                        />
-                      </div>
-                    ))}
-                  </Carousel>
-                  {/* <img width="300px" height="200px" src={API_URL + '/' + item.images[0]} /> */}
-                  <p className="m-2 ">
-                    {" "}
-                    {item.pname} | <span className="likeCatogary2"> {item.category}{" "}</span>
+                        {isReadMore ? "Read Less" : "Read More"}
+                      </span>
+                    )}
                   </p>
-                  <h3 className="m-2 text-danger">Rs.{item.price} </h3>
-                  <p className="m-2">
-                      {isReadMore ? item.pdesc : truncatedDescription}{" "}
-                      {item.pdesc.length > 50 && (
-                        <p
-                          onClick={() =>
-                            setReadMoreId(isReadMore ? null : item._id)
-                          }
-                          className="read-more-btn2"
-                        >
-                          {isReadMore ? "Read Less" : "Read More"}
-                        </p>
-                      )}
-                    </p>
-                    {loader && (
-          <div className="spinner-container">
-            <DotLoader color="#000" loading={loader} size={50} />
-          </div>
-        )}
-                 
                 </div>
               );
-            })}
+            })
+          )}
         </div>
       </div>
     </div>
